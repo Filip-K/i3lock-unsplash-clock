cf931c5c757c8bae213096f95b48c6e02d2dc1d5
Filip K <filip.bpk@gmail.com>
2021-05-11 19:42:09 +0200

    Outline to improve visibility on bright backgrounds.

--- i3lock-unsplash-clock.sh
+++ i3lock-unsplash-clock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -qO- https://picsum.photos/2560/1440 | convert - RGB:- | i3lock --clock --no-unlock-indicator --ignore-empty-password --timestr="%H%M" --datestr="" --timesize=256 --timecolor=ffffffb0 --raw 2560x1440:rgb --image /dev/stdin
+wget -qO- https://picsum.photos/2560/1440 | convert - RGB:- | i3lock --clock --no-unlock-indicator --ignore-empty-password --timestr="%H%M" --datestr="" --timesize=256 --timecolor=ffffffb0 --timeoutlinewidth=0.5 --timeoutlinecolor=0f0000b0 --raw 2560x1440:rgb --image /dev/stdin
